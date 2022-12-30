093c57d2edb306ded20b7a0e82d88b3c488c03ab
MeReda <medredaben14@gmail.com>
2022-12-30 18:19:42 +0100

    add form popup to utilisateurs page

--- utilisateurs page/script/script.js
+++ utilisateurs page/script/script.js
@@ -1,1 +1,19 @@
-// chage just the script.js file witch is this file
+// Begin display form on click on add button
+const addButton = document.querySelector(".add-btn"),
+    formPopup = document.querySelector(".form-popup");
+
+addButton.addEventListener("click", () => {
+    formPopup.classList.remove("hidden")
+    
+    const closeButton = document.querySelector(".close-btn")
+    closeButton.addEventListener("click", () => {
+        formPopup.classList.add("hidden")
+    })
+})
+// End display form on click on add button
+
+// Begin add Elements to table body
+
+
+
+// End add Elements to table body
