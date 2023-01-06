c70d4327bb0fbcf7861c59cc514c280d8cb085b7
MeReda <medredaben14@gmail.com>
2023-01-06 23:26:42 +0100

    add priviliges to admin and server

--- reservation page/script/script.js
+++ reservation page/script/script.js
@@ -1,3 +1,31 @@
+// Start privileges script for diff users
+
+const stockBtn = document.querySelector(".stock-btn"),
+    personnelsBtn = document.querySelector(".personnels-btn"),
+    reservationBtn = document.querySelector(".reservation-btn"),
+    menuBtn = document.querySelector(".menu-btn"),
+    utilisateursBtn = document.querySelector(".utilisateurs-btn");
+
+console.log(stockBtn)
+console.log(window.localStorage.group);
+
+window.onload = () => {
+    if (window.localStorage.group == "2") {
+        console.log("look above")
+        stockBtn.classList.add("disabled");
+        personnelsBtn.classList.add("disabled");
+        menuBtn.classList.add("disabled");
+        utilisateursBtn.classList.add("disabled");
+    } else if (window.localStorage.group == "3") {
+        stockBtn.classList.add("disabled");
+        personnelsBtn.classList.add("disabled");
+        reservationBtn.classList.add("disabled");
+        utilisateursBtn.classList.add("disabled");
+    }
+};
+
+// Start privileges script for diff users
+
 const popupContainer = document.querySelector(".reservation-container"),
     tableImg = document.querySelector(".reservation-img");
 
